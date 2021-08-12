79e1f66573c476a3ae08fd7910d62d86474551b7
Capri19971022 <88848404+Capri19971022@users.noreply.github.com>
2021-08-13 02:20:24 +0900

    Add files via upload

---
+++ comon/js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', (event) => {
+			document.getElementById('button1').addEventListener('click', () => {
+        // content3の表示位置を取得				var content3 = document.getElementById('content3');				var content_position = content3.getBoundingClientRect();
+        // content3へ移動				window.scrollTo( 0, content_position.top);
+			});		});
